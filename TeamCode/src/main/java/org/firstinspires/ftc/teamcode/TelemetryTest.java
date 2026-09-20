30a5e5afb4ca06820b7706ad09cf977ea42f399c
hemmithundhanasekaran-lab <hemmithun.dhanasekaran@gmail.com>
2026-09-20 11:47:47 -0500

    refix new code of sending telemetry to driver hub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
@@ -2,12 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-;
 @TeleOp
 public class TelemetryTest extends OpMode {
 
     @Override
     public void init() {
+
         telemetry.addData("Hello", "Decagonators");
     }
 
