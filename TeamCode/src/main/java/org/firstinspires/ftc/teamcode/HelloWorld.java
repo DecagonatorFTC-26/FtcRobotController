38b0ea5defe5f367f4480bedbd6a22db9fcd97f3
Dhanya <reachdms21@gmail.com>
2026-09-23 18:11:55 -0500

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -2,15 +2,20 @@ package org.firstinspires.ftc.teamcode;//this tells location the code is in
 
 
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;    // tells what other packages to use
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;    // packages have extra built in code that you can call
+import com.qualcomm.robotcore.eventloop.opmode.OpMode; // import means its telling java your bring something into this file
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp; // the other parts tell what specific package you wnt to bring
 
-@TeleOp
-public class HelloWorld extends OpMode {
+@TeleOp   // means this calss is a teleop program and it puts it in the teleop section
+public class HelloWorld extends OpMode {  //
     @Override
     public void init() {
         telemetry.addData("Hello", "World");
 
+
+    }
+
+    @Override
+    public void loop() {
 
     }
 
