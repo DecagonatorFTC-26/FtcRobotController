ab481497d908e33a8c45fed63b155ddb9f0d28c8
RiteshJCT <ritesh.jct@gmail.com>
2026-09-23 18:21:39 -0500

    FTC for Java Practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learnjavaforftc/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learnjavaforftc/HelloWorld.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.learnJavaForFTC;
+package org.firstinspires.ftc.teamcode.learnjavaforftc;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
